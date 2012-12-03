0809d2af3438e130a71e210a5ee48291f7074f0a
Derek <heyderek@dereknelson.net>
2012-12-03 11:52:11 -0800

    Added page template.

---
+++ page.php
@@ -0,0 +1,26 @@
+<?php get_header(); ?>
+      <section class="content">
+        <div class="wrapper">
+          <div class="primary">
+            <?php while(have_posts()) : the_post(); ?>
+            <h3><?php the_title(); ?></h3>
+            <?php the_content(); ?>
+            <?php endwhile; ?>
+          </div><!-- /.primary -->
+          <div class="secondary">
+            <aside>
+              <h3 class="ribbon"><strong class="ribbon-content">Heading 1</strong></h3>
+              <ul>
+                <li>List Item</li>
+                <li>List Item</li>
+                <li>List Item</li>
+                <li>List Item</li>
+                <li>List Item</li>
+                <li>List Item</li>
+                <li>List Item</li>
+              </ul>
+            </aside>
+          </div><!-- /.secondary -->
+        </div><!-- /.wrapper -->
+      </section><!-- /.content -->
+    <?php get_footer(); ?>
